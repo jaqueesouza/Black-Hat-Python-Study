95604edbdcbe97539de7bfaa99757f6730fddce8
Jaque <131463413+jaqueesouza@users.noreply.github.com>
2024-12-19 16:34:20 -0300

    Update Cliente_UDP.py

--- chapter2/Cliente_UDP.py
+++ chapter2/Cliente_UDP.py
@@ -1,16 +1,16 @@
 import socket
 
 target_host = "127.0.0.1"
-target_port = 9997
+target_port = 80
 
 # criar um objetivo socket
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# enviar alguns dados
+# enviar dados
 client.sendto(b"AAABBBCCC",(target_host,target_port))
 
-# receber alguns dados
+# receber dados
 data, addr = client.recvfrom(4096)
 
-print(data.decode())
 client.close()
+print(data.decode())
